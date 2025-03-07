146b0ff5ffe60305a34e6428052a24f6b7908fa1
poornavishnu <4vishnug@gmail.com>
2025-03-07 03:07:48 -0500

    no change to code

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-vishnu-123456" # Replace with your actual S3 bucket name
+    bucket         = "terraform-state-vishnu-123456" 
     key            = "terraform.tfstate"
     region         = "us-east-2"
     # dynamodb_table = "terraform-lock" # Optional, for state locking
